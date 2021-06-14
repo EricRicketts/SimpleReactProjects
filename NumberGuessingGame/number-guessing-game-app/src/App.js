abec9ef1500f441ada9d77e13390e0980eed14ac
EricRicketts <eric_ricketts@me.com>
2021-06-14 14:16:46 -0400

    form component

--- NumberGuessingGame/number-guessing-game-app/src/App.js
+++ NumberGuessingGame/number-guessing-game-app/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 
+function Form (props) {
+  return (
+    <form>
+      <fieldset>
+        <input type="text" id="guess"/>
+        <input type="submit" value="Guess"/>
+      </fieldset>
+    </form>
+  );
+}
 function App() {
   return (
     <main>
       <h1>Number Guessing Game</h1>
       <p>Guess an integer number from 1 to 100</p>
-      <form>
-        <fieldset>
-          <input type="text" id="guess"/>
-          <input type="submit" value="Guess"/>
-        </fieldset>
-      </form>
+      <Form className="form-guess"/>
       <a href="#">New Game</a>
     </main>
   );
