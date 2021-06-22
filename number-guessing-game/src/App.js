9d673ecfe1601bfe2cef6179d295f1393c4944ea
EricRicketts <eric_ricketts@me.com>
2021-06-22 14:55:00 -0400

    Hooraygit status  I got the bare bones application working using classes

--- number-guessing-game/src/App.js
+++ number-guessing-game/src/App.js
@@ -1,6 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
+class NumberGuessingForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <form action="">
+        <fieldset>
+          <input type="text" id="guess" maxLength={3}/>
+          <input type="submit"/>
+        </fieldset>
+      </form>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <main>
+        <h1>Number Guessing Game</h1>
+        <p>Loading...</p>
+        <NumberGuessingForm />
+        <a href="#">New Game</a>
+      </main>
+    );
+  }
+}
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +54,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
