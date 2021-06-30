70655734f02b1f813e56a1ca1812f9e895f53352
EricRicketts <eric_ricketts@me.com>
2021-06-30 04:48:28 -0400

    first simple calculator application test, test result paragraph

--- simple-arithmetic-calculator-app/src/App.js
+++ simple-arithmetic-calculator-app/src/App.js
@@ -1,24 +1,27 @@
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class CalculatorResult extends React.Component {
+  render() {
+    return (
+      <p>{this.props.calculatorResult}</p>
+    );
+  }
+}
+
+class CalculatorForm extends React.Component {
+   render() {
+     return (
+        <form onSubmit={this.props.handleSubmit}>
+
+        </form>
+     );
+   }
+}
+
+class  App extends React.Component {
+
 }
 
 export default App;
+export { CalculatorResult, CalculatorForm };
--- simple-arithmetic-calculator-app/src/App.test.js
+++ simple-arithmetic-calculator-app/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent, getByTestId } from '@testing-library/react';
+import * as React from 'react';
+import App, {
+ CalculatorResult,
+ CalculatorForm
+} from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App test', function () {
+ describe('CalculatorResult test', function () {
+  test('renders the result passed to it', () => {
+    render(<CalculatorResult calculatorResult={"5"} />);
+    let p = screen.getByText('5');
+    expect(p.constructor.name).toBe('HTMLParagraphElement');
+  });
+ });
 });
