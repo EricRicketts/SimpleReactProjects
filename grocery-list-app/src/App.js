4145bae9d8af9d4625c35be908306d34a8c5d5f9
EricRicketts <eric_ricketts@me.com>
2021-07-06 09:40:04 -0400

    finish grocery application along with its tests

--- grocery-list-app/src/App.js
+++ grocery-list-app/src/App.js
@@ -5,7 +5,7 @@ class GroceryList extends React.Component {
   render() {
     return (
       <ul id="groceryList" data-testid="groceryList">
-        {this.props.list.map((item, index) => <li key={index}>{`${item.quantity} ${item.name}`}</li>)}
+        {this.props.list.map((obj, index) => <li key={index}>{`${obj.quantity} ${obj.description}`}</li>)}
       </ul>
     );
   }
@@ -19,16 +19,20 @@ class GroceryListEntryForm extends React.Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    const description = event.target.elements.description.value;
+    const quantity = event.target.elements.quantity.value;
+    this.props.onSubmitHanlder(quantity, description);
+    event.target.reset();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} >
+      <form onSubmit={this.handleSubmit} data-testid="groceryListEntryForm" >
         <fieldset>
           <h2>Add an item</h2>
           <div className="groceryItemDescription">
-            <label htmlFor="item">Item name</label>
-            <input type="text" id="item" name="item" data-testid="item" />
+            <label htmlFor="description">Description</label>
+            <input type="text" id="description" name="description" data-testid="description" />
           </div>
           <div className="groceryItemDescription">
             <label htmlFor="quantity">Quantity</label>
@@ -42,15 +46,23 @@ class GroceryListEntryForm extends React.Component {
 }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { quantity: '', description: '' };
+    this.appHandleSubmit = this.appHandleSubmit.bind(this);
+    this.groceryList = [];
+  }
+
+  appHandleSubmit(quantity, description) {
+    const obj = { quantity, description }
+    this.setState(obj);
+    this.groceryList.push(obj);
+  }
   render() {
-    const list = [
-      { quantity: "1", name: "Peanut Butter" },
-      { quantity: "2", name: "Strawberry Jelly" },
-      { quantity: "1", name: "Loaf Of Bread" }
-    ];
+    const list = this.groceryList;
     return (
       <>
-        <GroceryListEntryForm />
+        <GroceryListEntryForm onSubmitHanlder={this.appHandleSubmit}/>
         <div id="groceryListContainer">
           <h2>Groceries</h2>
           <GroceryList list={list} />
@@ -61,3 +73,4 @@ class App extends React.Component {
 }
 
 export default App;
+export { GroceryList, GroceryListEntryForm };
--- grocery-list-app/src/App.test.js
+++ grocery-list-app/src/App.test.js
@@ -1,8 +1,76 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App, {
+  GroceryList,
+  GroceryListEntryForm
+} from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Test Grocery List Application', function () {
+  let results, expected, form, itemDescription, itemQuantity, groceryList;
+  describe('Grocery List Component', function () {
+    test('should produce an unordered list', function () {
+      const list = [
+        { quantity: "1", description: "Peanut Butter" },
+        { quantity: "2", description: "Strawberry Jelly" },
+        { quantity: "1", description: "Loaf Of Bread" }
+      ];
+      render(<GroceryList list={list} />);
+      const unorderedList = screen.getByTestId('groceryList');
+      expected = [3, true, '1 Peanut Butter', '2 Strawberry Jelly', '1 Loaf Of Bread'];
+      results = [unorderedList.childElementCount];
+      const listItems = unorderedList.children;
+      const allAreListItems = Array.from(listItems).every(listItem => listItem.constructor.name === 'HTMLLIElement');
+      results.push(allAreListItems)
+      Array.from(listItems).forEach(listItem => results.push(listItem.textContent));
+      expect(results).toEqual(expected);
+    });
+  });
+
+  describe('Grocery List Entry From Component', function () {
+    let submitHandler;
+    beforeEach(() => {
+      submitHandler = jest.fn();
+      render(<GroceryListEntryForm onSubmitHanlder={submitHandler} />);
+      form = screen.getByTestId('groceryListEntryForm');
+      itemDescription = screen.getByTestId('description');
+      itemQuantity = screen.getByTestId('quantity');
+      itemDescription.value = 'Foo';
+      itemQuantity.value = '1';
+    });
+
+    test('submit handler called on submission', function () {
+      fireEvent.submit(form);
+      expect(submitHandler).toHaveBeenCalledTimes(1);
+    });
+
+    test('the form clears itself upon submission', function () {
+      expected = ['Foo', '1', '', ''];
+      results = [itemDescription.value, itemQuantity.value];
+      fireEvent.submit(form);
+      results.push(itemDescription.value, itemQuantity.value);
+      expect(results).toEqual(expected);
+    });
+  });
+
+  describe('Top Level App Component', function () {
+    beforeEach(() => {
+      render(<App />);
+      form = screen.getByTestId('groceryListEntryForm');
+      itemDescription = screen.getByTestId('description');
+      itemQuantity = screen.getByTestId('quantity');
+      groceryList = screen.getByTestId('groceryList');
+    });
+
+    test('add items to grocery list', function () {
+      expected = [0, 2, '1 Loaf of Bread', '2 Jars of Peanut Butter'];
+      results = [groceryList.childElementCount];
+      [itemDescription.value, itemQuantity.value] = ['Loaf of Bread', '1'];
+      fireEvent.submit(form);
+      [itemDescription.value, itemQuantity.value] = ['Jars of Peanut Butter', '2'];
+      fireEvent.submit(form);
+      results.push(groceryList.childElementCount);
+      const liElements = Array.from(groceryList.children);
+      results.push(liElements[0].textContent, liElements[1].textContent);
+      expect(results).toEqual(expected);
+    });
+  });
 });
