f8ff91a772bdc95fa55c4fc5df1a004e4b3a8120
Eric Ricketts <eric_ricketts@me.com>
2022-07-03 07:12:35 -0400

    first form with no styling

---
+++ hooks-baisc-form/basic_form/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {ContactForm} from "./components/contact_form";
+
+function App() {
+  return (
+    <div className="App">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
