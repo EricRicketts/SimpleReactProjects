ab45697c6f3042e8776d6f7595eb779858e207e5
EricRicketts <eric_ricketts@me.com>
2022-07-07 14:44:00 -0400

    tests demonstrating basic form usage initialization

--- hooks-baisc-form/basic_react_hook_form_library_use/src/App.test.js
+++ hooks-baisc-form/basic_react_hook_form_library_use/src/App.test.js
@@ -3,10 +3,35 @@ import { render, screen } from '@testing-library/react';
 import "@testing-library/jest-dom";
 import BasicUsageForm from './components/basic_usage_form';
 
-describe("Test the first react hook form example", function() {
+describe("Test Basic Usage Form default render", function() {
 
-  test("Should render the basic fields", () => {
-    render(<BasicUsageForm />);
-    expect(screen.getByRole("textbox", { name: /^example$/i} )).toBeInTheDocument();
+  test("Should render the example label", function() {
+    render(<BasicUsageForm/>);
+    expect(screen.getByLabelText("Example")).toBeInTheDocument();
+  });
+
+  test("Should render the example input textbox", function() {
+    render(<BasicUsageForm/>);
+    expect(screen.getByRole("textbox", { name: /^example$/i })).toBeInTheDocument();
+  });
+
+  test("Should render the example required label", function() {
+    render(<BasicUsageForm/>);
+    expect(screen.getByLabelText("Example Required")).toBeInTheDocument();
+  });
+
+  test("Should render the example required textbox", function() {
+    render(<BasicUsageForm/>);
+    expect(screen.getByRole("textbox", { name: /Required/i })).toBeInTheDocument();
+  });
+
+  test("Should render the submit button", function() {
+    render(<BasicUsageForm/>);
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+
+  test("Should render default value of test in the example input", function() {
+    render(<BasicUsageForm/>);
+    expect(screen.getByRole("textbox", { name: /^example$/i })).toHaveValue("test");
   });
 });
