7dc05b54b66a440f95fe4e9c963404cc686594e0
Eric Ricketts <eric_ricketts@me.com>
2022-07-09 18:29:49 -0400

    set the input value on the input text box element

--- react-hook-form-library-examples/register_fields/src/App.test.js
+++ react-hook-form-library-examples/register_fields/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { storeData, RegisterFields } from './components/register_fields';
 
@@ -13,8 +13,23 @@ describe("Register Fields Initialization", () => {
     expect(screen.getByLabelText("First Name")).toBeEmptyDOMElement();
   });
 
-  test("should have placeholder in select input", () => {
+  test("Should have placeholder in select input", () => {
     render(<RegisterFields/>);
     expect(screen.getByLabelText("Gender")).toHaveDisplayValue("Select Gender");
+  });
+});
+
+describe("Register Fields Form Inputs", () => {
+  afterEach(() => {
+    storeData.submitArray = [];
+  });
+
+  test("Should set the text input value", () => {
+    render(<RegisterFields/>);
+    fireEvent.change(screen.getByLabelText("First Name"),
+      { target: { value: 'FizzBuzz'}
+    });
+    const input = screen.getByLabelText("First Name");
+    expect(input).toHaveDisplayValue("FizzBuzz");
   });
 });
