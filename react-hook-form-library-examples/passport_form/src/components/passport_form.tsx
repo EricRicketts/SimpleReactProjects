911fdfe23e6b815b6075fe6e9811cae95771db18
EricRicketts <eric_ricketts@me.com>
2022-07-20 17:57:35 -0400

    wrap passport types and sizes in a form control element

--- react-hook-form-library-examples/passport_form/src/components/passport_form.tsx
+++ react-hook-form-library-examples/passport_form/src/components/passport_form.tsx
@@ -1,13 +1,25 @@
 import {
-  Container
+  Container,
+  Button,
+  VStack,
 } from '@chakra-ui/react';
+import {useForm} from 'react-hook-form';
 import PassportTypesAndSizes from './passport_types_and_sizes';
 
 const PassportForm = () => {
+  const {handleSubmit} = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data)
+  }
   return (
-    <Container maxW='container.xl'>
-      <PassportTypesAndSizes/>
-    </Container>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Container maxW='container.xl'>
+        <VStack>
+          <PassportTypesAndSizes/>
+          <Button colorScheme='blue' size='md' type='submit'>Submit</Button>
+        </VStack>
+      </Container>
+    </form>
   );
 }
 
--- react-hook-form-library-examples/passport_form/src/components/passport_types_and_sizes.tsx
+++ react-hook-form-library-examples/passport_form/src/components/passport_types_and_sizes.tsx
@@ -3,37 +3,41 @@ import {
   VStack,
   RadioGroup,
   Radio,
-  Text,
+  FormControl, FormLabel, FormHelperText,
 } from '@chakra-ui/react';
 import React from 'react';
 
 const PassportTypesAndSizes = () => {
 
   return (
-    <VStack>
-      <Text as="b" fontSize='md' noOfLines={1}>
-        Select document(s) for which you are submitting fees:
-      </Text>
-      <RadioGroup defaultValue='USPassportBook'>
-        <HStack spacing={5}>
-          <Radio value='USPassportBook'>U.S. Passport Book</Radio>
-          <Radio value='USPassportCard'>U.S. Passport Card</Radio>
-          <Radio value='both'>Both</Radio>
-        </HStack>
-      </RadioGroup>
-      <Text fontSize='sm' noOfLines={1}>
-        The U.S. passport card is not valid for international travel.  See Instruction Page 3
-      </Text>
-      <RadioGroup defaultValue='regularBook'>
-        <HStack spacing={5}>
-          <Radio value='regularBook'>Regular Book (Standard)</Radio>
-          <Radio value='largeBook'>Large Book (Non-Standard)</Radio>
-        </HStack>
-      </RadioGroup>
-      <Text fontSize='sm' noOfLines={1}>
-        The large book is for frequent international travelers who need more visa pages.
-      </Text>
-    </VStack>
+    <FormControl as='fieldset'>
+      <VStack>
+        <FormLabel as='legend'>
+          Select document(s) for which you are submitting fees:
+        </FormLabel>
+        <RadioGroup defaultValue='USPassportBook'>
+          <HStack spacing={5}>
+            <Radio value='USPassportBook'>U.S. Passport Book</Radio>
+            <Radio value='USPassportCard'>U.S. Passport Card</Radio>
+            <Radio value='both'>Both</Radio>
+          </HStack>
+        </RadioGroup>
+        <FormHelperText>
+          The U.S. passport card is not valid for international travel.  See Instruction Page 3
+        </FormHelperText>
+      </VStack>
+      <VStack>
+        <RadioGroup defaultValue='regularBook'>
+          <HStack spacing={5}>
+            <Radio value='regularBook'>Regular Book (Standard)</Radio>
+            <Radio value='largeBook'>Large Book (Non-Standard)</Radio>
+          </HStack>
+        </RadioGroup>
+        <FormHelperText>
+          The large book is for frequent international travelers who need more visa pages.
+        </FormHelperText>
+      </VStack>
+    </FormControl>
   );
 }
 
