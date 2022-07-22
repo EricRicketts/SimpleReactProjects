8ccd4b92a8760d860056aab3502ae90c9331eb83
EricRicketts <eric_ricketts@me.com>
2022-07-22 16:45:56 -0400

    ran some experiments with register names and useState names

--- react-hook-form-library-examples/passport_form/src/components/passport_application_form.tsx
+++ react-hook-form-library-examples/passport_form/src/components/passport_application_form.tsx
@@ -14,28 +14,33 @@ import {
 } from '@chakra-ui/react';
 
 const PassportApplicationForm = () => {
-  const [type, setType] = React.useState('book');
+  const [passportType, setPassportType] = React.useState('book');
   const {
     handleSubmit,
     register,
     reset,
     formState,
     formState: { errors, isSubmitSuccessful }
-  } = useForm({
-    mode: 'onTouched',
-    defaultValues: {
-      passportType: 'book'
-    }
-  });
+  } = useForm({ mode: 'onTouched' });
   const onSubmit = (values: any): void => {
     console.log(values);
   }
-  
+ 
+  React.useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      console.log("successful submit");
+      reset({
+        passportType: 'book'
+      });
+      setPassportType('book');
+      console.log(passportType)
+    }
+  }, [isSubmitSuccessful, reset]);
   return (
     <form action="" onSubmit={handleSubmit(onSubmit)}>
       <VStack marginLeft='5px' align='left' spacing={5}>
         <FormControl>
-          <RadioGroup onChange={setType} value={type}>
+          <RadioGroup onChange={setPassportType} value={passportType}>
             <HStack spacing={5}>
               <Radio {...register('passportType')} value='book'>U.S. Passport Book</Radio>
               <Radio {...register('passportType')} value='card'>U.S. Passport Card</Radio>
