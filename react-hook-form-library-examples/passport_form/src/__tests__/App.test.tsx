af1ae723eaa0c3f9f06912d6b0b9c356f793e039
Eric Ricketts <eric_ricketts@me.com>
2022-07-23 11:10:38 -0400

    after refactoring test selecting passport types and passport book sizes

--- react-hook-form-library-examples/passport_form/src/__tests__/App.test.tsx
+++ react-hook-form-library-examples/passport_form/src/__tests__/App.test.tsx
@@ -17,4 +17,49 @@ describe('Test Suite for entire Form', () => {
       expect(screen.getByLabelText(/Regular Book/i)).toBeChecked();
     });
   });
+
+  describe('Test Passport Types and Sizes', () => {
+
+    test('should be able to select passport card', async () => {
+      render(<PassportApplicationForm/>);
+      const user = userEvent.setup();
+      const passportCard = screen.getByLabelText(/Passport Card/i);
+      await user.click(passportCard);
+      expect(passportCard).toBeChecked();
+    });
+
+    test('should be able to select both passport book and card', async () => {
+      render(<PassportApplicationForm/>);
+      const user = userEvent.setup();
+      const bothBookAndCard = screen.getByLabelText(/Both/i);
+      await user.click(bothBookAndCard);
+      expect(bothBookAndCard).toBeChecked();
+    });
+
+    test('should be able to toggle to select passport book', async () => {
+      render(<PassportApplicationForm/>);
+      const user = userEvent.setup();
+      await user.click(screen.getByLabelText(/Passport Card/i));
+      await user.click(screen.getByLabelText(/Passport Book/i));
+      const passportBook = screen.getByLabelText(/Passport Book/i)
+      expect(passportBook).toBeChecked();
+    });
+
+    test('should be able to select large size book', async () => {
+      render(<PassportApplicationForm/>);
+      const user = userEvent.setup();
+      await user.click(screen.getByLabelText(/Large Book/i));
+      const largeSizeBook = screen.getByLabelText(/Large Book/i)
+      expect(largeSizeBook).toBeChecked();
+    });
+
+    test('should be able to toggle to regular size book', async () => {
+      render(<PassportApplicationForm/>);
+      const user = userEvent.setup();
+      await user.click(screen.getByLabelText(/Large Book/i));
+      await user.click(screen.getByLabelText(/Regular Book/i));
+      const regularSizeBook = screen.getByLabelText(/Regular Book/i)
+      expect(regularSizeBook).toBeChecked();
+    });
+  });
 });
