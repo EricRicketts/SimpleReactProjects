c80f52e3f541bef072a77faa5ef8b7699c226076
Eric Ricketts <eric_ricketts@me.com>
2022-07-23 06:53:02 -0400

    new suite of tests, radio button defaults

--- react-hook-form-library-examples/passport_form/src/__tests__/App.test.tsx
+++ react-hook-form-library-examples/passport_form/src/__tests__/App.test.tsx
@@ -1,10 +1,20 @@
 import React from "react"
 import { screen, render } from "@testing-library/react"
 import userEvent from '@testing-library/user-event'
+import PassportApplicationForm from "../components/passport_application_form";
 
 describe('Test Suite for entire Form', () => {
-  
-  test('should get all tests to run', () => {
-    expect(1).toBe(1);
+  let expected, results;
+
+  describe('Test Form defaults', () => {
+    test('should have passport book as default', () => {
+      render(<PassportApplicationForm/>);
+      expect(screen.getByLabelText(/Passport Book/i)).toBeChecked();
+    });
+
+    test('should have regular size as default', () => {
+      render(<PassportApplicationForm/>);
+      expect(screen.getByLabelText(/Regular Book/i)).toBeChecked();
+    });
   });
 });
