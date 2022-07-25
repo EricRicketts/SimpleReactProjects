cbfd30d6aa45e7043db55ba4bbb3298833f4aa92
Eric Ricketts <eric_ricketts@me.com>
2022-07-25 04:33:03 -0400

    check in what I hae

--- react-hook-form-library-examples/passport_form/src/components/passport_application_form.tsx
+++ react-hook-form-library-examples/passport_form/src/components/passport_application_form.tsx
@@ -8,12 +8,15 @@ import {
   FormLabel,
   FormHelperText,
   FormErrorMessage,
+  Input,
   Radio,
   RadioGroup,
   Button
 } from '@chakra-ui/react';
 
 const PassportApplicationForm = () => {
+  const nameRegex = /^[A-Za-z]+$/;
+  const patternErrorMessage: string = 'Must be at least one letter';
   const [passportType, setPassportType] = React.useState('book');
   const [passportSize, setPassportSize] = React.useState('regular');
   const {
@@ -22,24 +25,34 @@ const PassportApplicationForm = () => {
     reset,
     formState,
     formState: { errors, isSubmitSuccessful }
-  } = useForm({ mode: 'onTouched' });
+  } = useForm({ mode: 'onTouched', defaultValues: {
+      'lastName': '',
+      'firstName': '',
+      'middleName': '',
+      'passportType': 'book',
+      'passportSize': 'regular',
+    }});
   const onSubmit = (values: any): void => {
     console.log(values);
   }
- 
+
   React.useEffect(() => {
     if (formState.isSubmitSuccessful) {
       console.log("successful submit");
       reset({
         passportType: 'book',
-        passportSize: 'regular'
+        passportSize: 'regular',
+        firstName: '',
+        lastName: '',
+        middleName: '',
       });
       setPassportType('book');
       setPassportSize('regular')
       console.log(passportType)
       console.log(passportSize)
+      console.log(errors);
     }
-  }, [isSubmitSuccessful, reset]);
+  }, [errors, isSubmitSuccessful, reset]);
   return (
     <form action="" onSubmit={handleSubmit(onSubmit)}>
       <VStack marginLeft='5px' align='left' spacing={5}>
@@ -59,6 +72,52 @@ const PassportApplicationForm = () => {
               <Radio {...register('passportSize')} value='large'>Large Book (Non-Standard)</Radio>
             </HStack>
           </RadioGroup>
+        </FormControl>
+        <FormControl>
+          <VStack marginLeft='5px' align='left' spacing={5}>
+            <VStack align='left'>
+              <FormLabel><strong>1. Name</strong> Last</FormLabel>
+              <Input {...register('lastName', {
+                required: {
+                  value: true,
+                  message: 'last name is required'
+                },
+                pattern: {
+                  value: nameRegex,
+                  message: patternErrorMessage
+                }
+              })}/>
+              {errors.lastName && <FormErrorMessage>errors.lastName.message</FormErrorMessage>}
+            </VStack>
+            <HStack spacing={5}>
+              <VStack>
+                <FormLabel>First</FormLabel>
+                <Input {...register('firstName', {
+                  required: {
+                    value: true,
+                    message: 'first name is required'
+                  },
+                  pattern: {
+                    value: nameRegex,
+                    message: patternErrorMessage
+                  }
+                })}/>
+              </VStack>
+              <VStack>
+                <FormLabel>Middle</FormLabel>
+                <Input {...register('middleName', {
+                  required: {
+                    value: true,
+                    message: 'middle name is required'
+                  },
+                  pattern: {
+                    value: nameRegex,
+                    message: patternErrorMessage
+                  }
+                })}/>
+              </VStack>
+            </HStack>
+          </VStack>
         </FormControl>
         <Button width='100px' type='submit' colorScheme='blue'>Submit</Button>
       </VStack>
