5d6394976d3a48203d8716846a7b6c9b40c3fea9
EricRicketts <eric_ricketts@me.com>
2022-07-27 12:40:25 -0400

    added ChakraProvider which enabled the form to be properly styled

--- react-hook-form-input-validate/src/App.tsx
+++ react-hook-form-input-validate/src/App.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
-import { Text, VStack } from "@chakra-ui/react";
+import { Text, VStack, ChakraProvider } from "@chakra-ui/react";
 // import InputName from "./components/input_name";
 import HandleErrorsExample from "./components/handle_errors_example";
 
 export const App = () => {
   return (
-    <VStack align="left">
-      <HandleErrorsExample />
-    </VStack>
+    <ChakraProvider>
+      <VStack marginLeft={5} align="left">
+        <HandleErrorsExample />
+      </VStack>
+    </ChakraProvider>
   );
 };
