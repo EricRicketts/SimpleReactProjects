261aa1c2149070a9217ce6f2c3b9a14e0e730884
EricRicketts <eric_ricketts@me.com>
2022-08-01 15:25:32 -0400

    had to align mac desktop with Peraton desktop

--- react-hook-form-input-validate/src/components/passport_application.tsx
+++ react-hook-form-input-validate/src/components/passport_application.tsx
@@ -1,28 +1,35 @@
 import React from "react";
 import { InputNameTemplate, IFormValues } from "./input_name_template";
 import { useForm } from "react-hook-form";
-import {
-  VStack,
-  Button,
-} from "@chakra-ui/react";
+import { VStack, Button } from "@chakra-ui/react";
 
 const PassportApplication = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormValues>({ mode: "onTouched",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormValues>({
+    mode: "onTouched",
     defaultValues: {
-      "firstName": ''
-    }
+      lastName: "",
+    },
   });
 
   const onSubmit = (data: IFormValues) => console.log(data);
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <VStack marginLeft="10px" align="left">
-        <InputNameTemplate id={"lastName"} label={"Last Name"} register={register} errors={errors} />
+        <InputNameTemplate
+          id={"lastName"}
+          label={"Last Name"}
+          register={register}
+          errors={errors}
+        />
         <Button mt={4} color="teal" type="submit">
           Submit
         </Button>
       </VStack>
     </form>
   );
-}
+};
 export default PassportApplication;
